afc1e9d9dc72942c606ac8aa80ece89173308b18
Marc-Andre Moreau <marcandre.moreau@gmail.com>
2010-06-14 10:19:48 -0400

    added new admin control panel

---
+++ admin/admin.js
@@ -0,0 +1,126 @@
+
+function getData(params, callback, options)
+{
+	options = options || {};
+	options.type = options.type || "get";
+	var startTime = new Date();
+
+	function updateTimer() {
+		var stopTime = new Date();
+		var timeTaken = (stopTime-startTime)/1000
+		if (!$("#querySpeed").data("timeStore"))
+			$("#querySpeed").data("timeStore",[]);
+		var timeStore = $("#querySpeed").data("timeStore");
+		timeStore.push(timeTaken);
+		$("#querySpeed").text(Math.round(timeTaken*1000)/1000).attr("title",timeStore.length + " queries total. Avg run time: " + Math.round(average(timeStore)*1000)/1000);
+	}
+
+	selfRepeatCall = function() { getData(params, callback, options); }
+
+	if (arguments.length > 3 && arguments.length < 2)
+		return alert("Invalid call for getData, you need 2 params, you gave me " + arguments.length + "... Crashing!");
+
+	if (!params.action)
+		return alert("Can't get data without an action!");
+
+	$.ajax({"cache":"false", "type":options.type, "data":params, "dataType":"json", "url":"index.php"
+	, "success": function(response)
+	{
+		updateTimer();
+		if (!response)
+		{
+			alert('Network Error');
+		}
+		else if (response.error && response.error == "true")
+		{
+			alert('Unknown error occured.. Please try again. <hr>' + response.msg);
+		}
+		else
+		{
+			callback(response);
+		}
+	}
+	, "error":function(XMLHttpRequest, textStatus, errorThrown)
+	{
+		updateTimer();
+		if (textStatus == "parsererror")
+			alert("Not a valid JSON object. " + XMLHttpRequest.responseText);
+		else
+			alert('Network Error');
+	}
+
+	});
+}
+
+function authenticate(username, password, callback)
+{
+	getData({"action":"login","username":username,"password":password}, function(response){
+		if (response.loginError == "false") {
+			callback();
+		}
+		else {
+
+		}
+	}, {"type":"post"});
+}
+
+function logout()
+{
+	getData({"action":"logout"}, function(response){
+		$("#admin-tabs").hide();
+		$("#auth-status").hide();
+		loadStatus();
+	}, {"type":"post"});
+}
+
+function loadStatus()
+{
+	getData({"action":"status"}, function(response) {
+		if (response.authenticated == "true") {
+			$("#admin-tabs").tabs({
+				ajaxOptions: {
+					error: function(xhr, status, index, anchor) {
+						$(anchor.hash).html("Error loading tab!");
+					}
+				},
+				cookie: {
+					expires: 30
+				}
+			});
+			$("#admin-tabs").show();
+			$("#auth-status").show();
+		}
+		else {
+			$("#admin-login-dialog").dialog({
+				show:"puff",
+				hide:"puff",
+				modal: true,
+				resizable:false,
+				buttons: {
+					Ok: function(){
+						var modalWindow = $("#admin-login-dialog");
+						authenticate(modalWindow.find(".username").val(), modalWindow.find(".password").val(), function(){
+							modalWindow.dialog('close');
+							modalWindow.find(".password").val("");
+						});
+						loadStatus();
+					}
+				}
+			});
+		}
+	}, {"type":"post"});
+}
+
+function average(items)
+{
+	var sum = 0;
+	for (i = 0; i < items.length; i++)
+		sum += items[i];
+	return (sum / items.length);
+}
+
+$(function() {
+
+	loadStatus();
+
+});
---
+++ admin/index.php
@@ -0,0 +1,191 @@
+<?php
+	session_start();
+	$config = parse_ini_file("../config.ini", true);
+	$contextpath = $config['web']['contextpath'];
+	require "../inc/utilities.php";
+	require "../inc/db.php";
+
+        function login()
+        {
+		if ( $_SESSION['username'] && $_SESSION['auth'] == 'yes' ) {
+			$username = $_SESSION['username'];
+			/* Already authenticated */
+			print(json_encode(array("status"=>"ok","loginError"=>"false","reason"=>"")));
+			return true;
+		} else {
+			$username = preg_replace("/[^a-zA-Z0-9_ -]/", "", $_POST['username']);
+			$password = $_POST['password'];
+
+			if ( $username && $password ) {
+
+				$result = mysql_queryf("SELECT id FROM users WHERE name=%s AND password=SHA1(CONCAT(seed, %s)) LIMIT 1;", $username, $password);
+
+				if ( mysql_num_rows( $result ) > 0 ) {
+					/* Authentication successful */
+					$_SESSION['username'] = $username;
+					$_SESSION['auth'] = "yes";
+					$_SESSION['tab'] = "testreports";
+					print(json_encode(Array("status"=>"ok","loginError"=>"false","reason"=>"")));
+					return true;
+				} else {
+					/* Authentication failure */
+					print(json_encode(Array("status"=>"ok","loginError"=>"true","reason"=>"Wrong username or password")));
+					return true;
+				}
+			}
+		}
+        }
+
+	function logout()
+	{
+		$_SESSION['username'] = "";
+		$_SESSION['auth'] = "";
+		$_SESSION['tab'] = "";
+		session_write_close();
+		$status = Array("status"=>"ok");
+		print(json_encode($status));
+	}
+
+	function dataset_encode($dataset)
+	{
+		$i = 0;
+		$str = '{"aaData":[';
+		foreach ($dataset as $data)
+		{
+			if ($i++) $str .= ',';
+			$str .= '[';
+
+			$j = 0;
+			foreach ($data as $element)
+			{
+				if ($j++) $str .= ',';
+				$str .= "\"$element\"";
+			}
+			$str .= ']';
+		}
+		$str .= ']}';
+		return $str;
+	}
+
+	function dataset_encode_assoc($dataset)
+	{
+		$i = 0;
+		$str = '{"aaData":[';
+
+		foreach ($dataset as $key => $val)
+		{
+			if ($i++) $str .= ',';
+			$str .= "[\"$key\",\"$val\"]";
+		}
+
+		$str .= ']}';
+		return $str;
+	}
+
+	function getStatus()
+	{
+		$authenticated = "false";
+		$tab = "testreports";
+
+		if ( $_SESSION['username'] && $_SESSION['auth'] == 'yes' ) {
+			$authenticated = "true";
+		}
+
+		if ( $_SESSION['tab'] ) {
+			$tab = $_SESSION['tab'];
+		}
+
+		$status = Array("authenticated"=>$authenticated,"tab"=>$tab);
+		print(json_encode($status));
+	}
+
+	function getUserAgents()
+	{
+		$query = "SELECT name,engine,version,active,current FROM useragents";
+		$result = mysql_queryf($query);
+
+		$uas = Array();
+
+		$i = 0;
+		while ( $row = mysql_fetch_row($result) ) {
+			$uas[$i++] = $row;
+		}
+
+		echo dataset_encode($uas);
+	}
+
+	function getJobs()
+	{
+		$query = "SELECT name,user_id,status,updated,created FROM jobs";
+		$result = mysql_queryf($query);
+
+		$jobs = Array();
+
+		$i = 0;
+		while ( $row = mysql_fetch_row($result) ) {
+			$jobs[$i++] = $row;
+		}
+
+		echo dataset_encode($jobs);
+	}
+
+	function getSettings()
+	{
+		$query = "SELECT name,auth FROM users";
+		$result = mysql_queryf($query);
+		$settings_assoc = mysql_fetch_assoc($result);
+		echo dataset_encode_assoc($settings_assoc);
+	}
+
+	/* unauthenticated actions */
+	if (isset($_REQUEST["action"]))
+	{
+		switch ($_REQUEST["action"])
+		{
+			case("login"):
+				login();
+				exit();
+				break;
+
+			case("logout"):
+				logout();
+				exit();
+				break;
+
+			case("status"):
+				getStatus();
+				exit();
+				break;
+		}
+	}
+
+	if ( $_SESSION['username'] && $_SESSION['auth'] == 'yes' )
+	{
+		/* authenticated actions */
+		if (isset($_REQUEST["action"]))
+		{
+			switch ($_REQUEST["action"])
+			{
+				case("testreports"):
+					getJobs();
+					exit();
+					break;
+
+				case("jobcontrol"):
+					getJobs();
+					exit();
+					break;
+
+				case("useragents"):
+					getUserAgents();
+					exit();
+					break;
+
+				case("settings"):
+					getSettings();
+					exit();
+					break;
+			}
+		}
+	}
+?>
