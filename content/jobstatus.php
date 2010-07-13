75f59715d3809b361f754b816015c3611b18350d
Aaron Unger <aunger@concord.org>
2010-07-13 10:42:05 -0400

    Add browser OS info into the job status page.
    Refactor job status page (again) to make it easier to understand how the page is being generated.

--- content/jobstatus.php
+++ content/jobstatus.php
@@ -26,7 +26,7 @@
 
 	$job_id = preg_replace("/[^0-9]/", "", $_REQUEST['job_id']);
 
-	$result = mysql_queryf("SELECT jobs.name, jobs.status, users.name FROM jobs, users WHERE jobs.id=%u AND users.id=jobs.user_id;", $job_id);
+	$result = mysql_queryf("SELECT jobs.name, jobs.status, users.name as user_name FROM jobs, users WHERE jobs.id=%u AND users.id=jobs.user_id;", $job_id);
 
 	if ( $row = mysql_fetch_array($result) ) {
 		$job_name = $row[0];
@@ -34,54 +34,105 @@
 		$owner = ($row[2] == $_SESSION['username']);
 	}
 	
-	function generate_browser_header_row($browsers){
+	function generate_browser_header_row(&$browsers){
 	  $header = "<tr><th></th>\n";
-		$last_browser = array();
-		foreach ( $browsers as $browser ) {
-			if ( ! array_key_exists("id", $last_browser) || $last_browser["id"] != $browser["id"] ) {
-				$header .= '<th><div class="browser">' .
+		foreach ( $browsers as $browser => $oses ) {
+		  ksort($oses);
+		  foreach ($oses as $os => $browser) {
+				$header .= '<th>' . '<div class="browser ' . $browser["engine"] . ' ' . $browser["os"] . '">' .
 					'<img src="' . $GLOBALS['contextpath'] . '/images/' . $browser["engine"] .
 					'.sm.png" class="browser-icon ' . $browser["engine"] .
 					'" alt="' . $browser["name"] .
-					'" title="' . $browser["name"] .
+					'" title="' . $browser["name"] . ' (' . $browser["os"] . ')' .
 					'"/><span class="browser-name">' .
 					preg_replace('/\w+ /', "", $browser["name"]) . ', ' .
-					"</span></div></th>\n";
+					"</span></div><div class='browser-os'>" . $browser["os"] . "</div></th>\n";
 			}
 			$last_browser = $browser;
 		}
-		$header .= "</tr>\n";
 		return $header;
 	}
 	
-	function setup_useragents_for_run($run_id) {
+	function add_browser_info(&$browsers, $name, $engine, $id, $os) {
+	  // register the new browser config, if it's not already registered
+	  if (! $os || $os == '') {
+	    $os = 'unknown';
+	  }
+		$browser_info = array(
+			"name" => $name,
+			"engine" => $engine,
+			"id" => $id,
+			"os" => $os
+		);
+    if (! array_key_exists($name, $browsers)) {
+      $browsers[$name] = array();
+      $browsers[$name][$os] = $browser_info;
+	  } else {
+	    if ($os == "any") {
+	      // we already have a result for this useragent, so we don't need to add the default value
+	      return;
+	    }
+	    if (! array_key_exists($os, $browsers[$name])) {
+        $browsers[$name][$os] = $browser_info;
+  	  }
+	  }
+	}
+	
+	function setup_useragents_for_run($run_id, &$browsers) {
 	  $useragents = array();
 
-		$runResult = mysql_queryf("SELECT run_client.client_id as client_id, run_client.status as status, run_client.fail as fail, run_client.error as error, run_client.total as total, clients.useragent_id as useragent_id FROM run_client, clients WHERE run_client.run_id=%u AND run_client.client_id=clients.id ORDER BY useragent_id;", $run_id);
+		$runResult = mysql_queryf("SELECT run_client.client_id as client_id, run_client.status as status, run_client.fail as fail, run_client.error as error, run_client.total as total, clients.useragent_id as useragent_id, useragents.engine as browser, useragents.name as browsername, clients.os as os FROM run_client, clients, useragents WHERE run_client.run_id=%u AND run_client.client_id=clients.id AND clients.useragent_id=useragents.id ORDER BY useragent_id, os;", $run_id);
 
 		while ( $ua_row = mysql_fetch_assoc($runResult) ) {
-			if ( ! array_key_exists($ua_row['useragent_id'], $useragents) ) {
-				$useragents[ $ua_row['useragent_id'] ] = array();
+		  $key = $ua_row['useragent_id'] . '--' . ($ua_row['os'] == '' ? 'unknown' : $ua_row['os']);
+			if ( ! array_key_exists($key, $useragents) ) {
+				$useragents[ $key ] = array();
+				add_browser_info($browsers, $ua_row["browsername"], $ua_row["browser"], $ua_row["useragent_id"], $ua_row["os"]);
 			}
 
-			array_push( $useragents[ $ua_row['useragent_id'] ], $ua_row );
+			array_push( $useragents[ $key ], $ua_row );
 		}
+		
+		$additionalBrowsers = mysql_queryf("SELECT useragents.* FROM useragents, run_useragent, runs WHERE runs.id=%u AND runs.id=run_useragent.run_id AND run_useragent.useragent_id=useragents.id", $run_id);
+		while ( $row = mysql_fetch_assoc($additionalBrowsers)) {
+		  add_browser_info($browsers, $row["name"], $row["engine"], $row["id"], "any");
+		}
+		
 		return $useragents;
 	}
 	
-	function get_useragent_results($ua, $row) {
-	  $status = get_status2(intval($ua["status"]), intval($ua["fail"]), intval($ua["error"]), intval($ua["total"]));
-		return "<td class='$status " . $row["browser"] . "'><a href='" . $GLOBALS['contextpath'] . "/?state=runresults&run_id=" . $row["run_id"] . "&client_id=" . $ua["client_id"] . "'>" .
-			($ua["status"] == 2 ?
-				($ua["total"] < 0 ?
-					"Err" :
-					($ua["error"] > 0 ?
-						$ua["error"] :
-						($ua["fail"] > 0 ?
-							$ua["fail"] :
-							$ua["total"])))
-				: "") . "</a></td>\n";
+	function get_useragent_results($uas, $row, $browser, $max) {
+	  $out = '<td class="results ' . $browser["name"] . ' ' . $browser["os"] . '">';
+	  $count = count($uas);
+	  $result_size = $max/$count;
+	  if ($max == 1) {
+	    $result_size = 1.7;
+	  }
+	  foreach ($uas as $ua) {
+	    
+  	  $status = get_status2(intval($ua["status"]), intval($ua["fail"]), intval($ua["error"]), intval($ua["total"]));
+  		$out .= "<span class='$status " . $browser["name"] . ' ' . $browser["os"] . "' style='height: " . $result_size . "em;'><a href='" . $GLOBALS['contextpath'] . "/?state=runresults&run_id=" . $row["run_id"] . "&client_id=" . $ua["client_id"] . "'>" .
+  			($ua["status"] == 2 ?
+  				($ua["total"] < 0 ?
+  					"Err" :
+  					($ua["error"] > 0 ?
+  						$ua["error"] :
+  						($ua["fail"] > 0 ?
+  							$ua["fail"] :
+  							$ua["total"])))
+  				: "") . "</a></span>\n";
+		}
+		return $out . '</td>' . "\n";
 	}
+	
+  function get_max_results($useragents) {
+    $max = 0;
+    foreach ($useragents as $agent => $results) {
+      $count = count($results);
+      if ($count > $max) { $max = $count; }
+    }
+    return $max;
+  }
 
 ?>
 
@@ -99,61 +150,51 @@
 <table class="results"><tbody>
 <?php
 
-	$result = mysql_queryf("SELECT runs.id as run_id, runs.url as run_url, runs.name as run_name, useragents.engine as browser, useragents.name as browsername, useragents.id as useragent_id, run_useragent.status as status FROM run_useragent, runs, useragents WHERE runs.job_id=%u AND run_useragent.run_id=runs.id AND run_useragent.useragent_id=useragents.id ORDER BY run_id, browsername;", $job_id);
+	$result = mysql_queryf("SELECT runs.id as run_id, runs.url as run_url, runs.name as run_name FROM runs WHERE runs.job_id=%u ORDER BY run_id;", $job_id);
 
 	$last = "";
 	$output = "";
 	$browsers = array();
+	$runs = array();
 
+  // cache all of the report data
 	while ( $row = mysql_fetch_assoc($result) ) {
-	  // if we're on to a new run, set up the useragents and start up a new run row
-		if ( $row["run_id"] != $last ) {
-		  // if we're not just on the first row, close out the last run row
-		  if ($output != "") {
-		    $output .= "</tr>\n";
-		  }
-      $useragents = setup_useragents_for_run($row["run_id"]);
-			$output .= '<tr><th><a href="' . $row["run_url"] . '">' . $row["run_name"] . "</a></th>\n";
-		}
-
-    // register the browser if it hasn't already been
-    $browser_info = array(
-			"name" => $row["browsername"],
-			"engine" => $row["browser"],
-			"id" => $row["useragent_id"]
-		);
-		
-    if (! in_array($browser_info, $browsers)) {
-		  array_push( $browsers, $browser_info );
+	  if (array_key_exists($row["run_id"], $runs)) {
+	    // append the run info
+	  } else {
+	    $useragents = setup_useragents_for_run($row["run_id"], $browsers);
+	    $run_data = array();
+	    $run_data["useragents"] = $useragents;
+	    $run_data["run_url"] = $row["run_url"];
+	    $run_data["run_name"] = $row["run_name"];
+	    $runs[$row["run_id"]] = $run_data;
 	  }
+  }
+  
+  ksort($browsers);
 
-		#echo "<li>" . $row["browser"] . " (" . get_status(intval($row["status"])) . ")<ul>";
+  // now print out the table
+  echo generate_browser_header_row($browsers);
 
-		$last_browser = -1;
-		if ( array_key_exists($row["useragent_id"], $useragents) ) {
-		  // this gets the first result sent back per user agent...
-		  // ???: Should we represent the other results? If so, how?
-			foreach ( $useragents[ $row["useragent_id"] ] as $ua ) {
-				if ( $last_browser != $ua["useragent_id"] ) {
-          $output .= get_useragent_results($ua, $row);
-				}
-				$last_browser = $ua["useragent_id"];
-			}
-		} else {
-			$output .= "<td class='notstarted notdone'>&nbsp;</td>\n";
-		}
+  foreach ($runs as $run) {
+    echo "<tr><th><a href='" . $run["run_url"] . "'>" . $run["run_name"] . "</a></th>\n";
+    $useragents = $run["useragents"];
+    $max = get_max_results($useragents);
+    foreach ($browsers as $browser => $oses) {
+      ksort($oses);
+      foreach ($oses as $browser) {
+        $key = $browser["id"] . '--' . $browser["os"];
+        if (array_key_exists($key, $useragents)) {
+          $run_results = $useragents[$key];
+          echo get_useragent_results($run_results, $run, $browser, $max);
+        } else {
+          echo "<td class='notstarted notdone " . $browser["name"] . " " . $browser["os"] . "'>&nbsp;</td>";
+        }
+      }
+    }
+    echo "</tr>";
+  }
 
-		#echo "</ul></li>";
-
-		$last = $row["run_id"];
-	}
-	
-	// finally generate the browser icon header row
-	$header = generate_browser_header_row($browsers);
-	$output = $header . $output;
-
-  // close out the final run
-	$output .= "</tr>\n";
-
-	echo "$output</tbody>\n</table>";
 ?>
+</tbody>
+</table>
